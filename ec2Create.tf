bca9ea645da5e7da313d221ca731bcddd4fffa8d
abdullahyilmaz0 <abdullahyilmaz0@gmail.com>
2024-12-04 10:28:05 +0100

    outputs added

--- ec2Create.tf
+++ ec2Create.tf
@@ -2,6 +2,9 @@
 resource "aws_instance" "firstServer" {
   ami           = "ami-0453ec754f44f9a4a"
   instance_type = "t2.micro"
-
 }
+output "ec2_instance" {
+  value = aws_instance.firstServer.public_ip
+}
+
 
