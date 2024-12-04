53af61a79830fba2b5fbc52c4840f0631bf45560
abdullahyilmaz0 <abdullahyilmaz0@gmail.com>
2024-12-04 11:04:19 +0100

    variables added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_name" {
+  default = "abd_test_instance"
+
+}
+variable "ec2_type" {
+  default = "t2.micro"
+
+}
+variable "ec2_ami" {
+  default = "ami-0453ec754f44f9a4a"
+
+}
+variable "s3_bucket_name" {
+  #default = "abd_test_s3Bucket"
+}
